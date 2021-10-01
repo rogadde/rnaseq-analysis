6fda0721bc02a19b3f42374eaac9c66a1be0c0cb
Rohini K Gadde <rkgadde@banjo.ucsd.edu>
2021-09-30 19:43:22 -0700

    Added args

--- bin/trimmomatic.sh
+++ bin/trimmomatic.sh
@@ -2,17 +2,19 @@
 #
 # Author: Rohini Gadde
 #   Reference: https://datacarpentry.org/wrangling-genomics/03-trimming/index.html
-# Usage: ../bin/trimmomatic.sh (run in data directory of project)
+# Usage: ../bin/trimmomatic.sh $1 $2 (run in data directory of project)
 #   This script loops through a directory of .fastq.gz files and trims adapter
 #   sequences and low-quality sequences.
+# 1 - Number of threads
+# 2 - File containing adapter/primer sequences
 # TODO: Make more general for future use. Create file structure instead of hard
 # coding existing paths.
 
 for file in fastq/*_1.fastq.gz; do
   read=$(basename "${file}" _1.fastq.gz)
-  trimmomatic PE -threads 10 -summary filtered/trimsum.txt \
+  trimmomatic PE -threads "$1" -summary filtered/trimsum.txt \
     fastq/"${read}"_1.fastq.gz fastq/"${read}"_2.fastq.gz \
-    filtered/"${read}"_1p.trim.fastq.gz filtered/"${read}"_1u.trim.fastq.gz \
-    filtered/"${read}"_2p.trim.fastq.gz filtered/"${read}"_2u.trim.fastq.gz \
-    ILLUMINACLIP:NexteraPE-PE.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:35
+    filtered/"${read}"_1P.trim.fastq.gz filtered/"${read}"_1U.trim.fastq.gz \
+    filtered/"${read}"_2P.trim.fastq.gz filtered/"${read}"_2U.trim.fastq.gz \
+    ILLUMINACLIP:"$2":2:30:10 SLIDINGWINDOW:4:20 MINLEN:25
 done
