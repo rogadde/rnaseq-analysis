abe231fe0481957ae2d003b379845def0cd6cb71
Rohini K Gadde <rkgadde@banjo.ucsd.edu>
2021-10-15 14:59:37 -0700

    Fixed path

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -5,8 +5,10 @@
 #   This script deletes unnecessary files generated earlier in this analysis.
 
 # Delete unsorted bam files
-for dir in STAR/SRR*; do
-  if [[ -e "${dir}Aligned.out.bam" ]]; then
-    rm -f "${dir}Aligned.out.bam"
+for dir in STAR/SRR*/; do
+  acc=$(basename "${dir}")
+  
+  if [[ -e "${dir}${acc}Aligned.out.bam" ]]; then
+    rm -f "${dir}${acc}Aligned.out.bam"
   fi
 done
