e067a87294b758ec18adfb76fa4ddd055a43d136
Rohini K Gadde <rkgadde@banjo.ucsd.edu>
2021-11-04 00:24:54 -0700

    Change file names

--- bin/duplication.r
+++ bin/duplication.r
@@ -20,21 +20,21 @@
 library(dupRadar)
 
 # get name patterns from command line
-args   <- commandArgs(TRUE)
+args <- commandArgs(TRUE)
 
 # usually, same GTF file as used in htseq-count
 gtf <- args[1]
 # 0(no)|1(yes)|2(reverse)
-stranded <- args[2]
+stranded <- as.integer(args[2])
 # is a paired end experiment
-paired   <- args[3]
+paired <- args[3]
 # number of threads to be used
-threads  <- args[4]
+threads <- as.integer(args[4])
 
 if(length(args) != 4) { 
   stop (paste0("Usage: ./dupRadar.sh <file.bam> <genes.gtf> ",
-               "<stranded=[0(no)|1(yes)|2(reverse)]> paired=[TRUE|FALSE] ",
-               "threads=1"))
+               "<0(no)|1(yes)|2(reverse)> <TRUE(PE)|FALSE(SE)> ",
+               "<threads>"))
 }
 
 if(!file.exists(gtf)) {
@@ -60,43 +60,44 @@ files <- list.files(path="./STAR", pattern="sortedByCoord", full.names=TRUE,
   recursive=TRUE)
   
 for (alignment in files) {
-  # analyze duprates and create plots
-  cat("Processing file ", bam, " with GTF ", gtf, "\n")
-  
   # coordinate-sorted bam file to analyze
   bam <- alignment
   outdir <- dirname(bam)
   
+  # analyze duprates and create plots
+  cat("Processing file ", bam, " with GTF ", gtf, "\n")
+  
   # mark duplicates with BamUtil
   bamDuprm <- markDuplicates(dupremover="bamutil",
-                 bam,
-                 path="/opt/bamUtil-master/bin",
-                 rminput=TRUE)
+                bam,
+                out=gsub("Aligned.sortedByCoord.out.bam", "_duprm.bam", bam),
+                path="/home/AD/rkgadde/miniconda3/pkgs/bamutil-1.0.15-h2e03b76_1/bin",
+                rminput=FALSE)
   
   # calculate duplication rate matrix
   dm <- analyzeDuprates(bamDuprm,
                         gtf,
                         stranded,
-                        paired,
+                        (paired == "TRUE"),
                         threads)
   
   # produce plots
   # duprate vs. expression smooth scatter
-  png(file=paste0(outdir,"/",gsub("(.*)\\.[^.]+","\\1",basename(bam)),"_dupRadar_drescatter.png"),
+  png(file=paste0(outdir,"/",gsub("_duprm.bam","",basename(bamDuprm)),"_dupRadar_drescatter.png"),
       width=1000, height=1000)
-  duprateExpDensPlot(dm, main=basename(bam))
+  duprateExpDensPlot(dm, main=basename(bamDuprm))
   dev.off()
   
   # expression histogram
-  png(file=paste0(outdir,"/",gsub("(.*)\\.[^.]+","\\1",basename(bam)),"_dupRadar_ehist.png"),
+  png(file=paste0(outdir,"/",gsub("_duprm.bam","",basename(bamDuprm)),"_dupRadar_ehist.png"),
       width=1000, height=1000)
   expressionHist(dm)
   dev.off()
   
   ## duprate vs. expression boxplot
-  png(file=paste0(outdir,"/",gsub("(.*)\\.[^.]+","\\1",basename(bam)),"_dupRadar_drebp.png"),
+  png(file=paste0(outdir,"/",gsub("_duprm.bam","",basename(bamDuprm)),"_dupRadar_drebp.png"),
       width=1000, height=1000)
   par(mar=c(10,4,4,2)+.1)
-  duprateExpBoxplot(dm, main=basename(bam))
+  duprateExpBoxplot(dm, main=basename(bamDuprm))
   dev.off()
 }
